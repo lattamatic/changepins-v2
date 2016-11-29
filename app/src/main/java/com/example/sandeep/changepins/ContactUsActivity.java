8f72809c63e79ff8b8555467015350f4746cad38
lattamatic <chituprolusandeep@gmail.com>
2016-11-29 19:20:26 +0530

    Added back button in ContactUs page

--- app/src/main/java/com/example/sandeep/changepins/ContactUsActivity.java
+++ app/src/main/java/com/example/sandeep/changepins/ContactUsActivity.java
@@ -3,6 +3,9 @@ package com.example.sandeep.changepins;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by sandeep on 24/11/16.
@@ -10,10 +13,22 @@ import android.support.v7.app.AppCompatActivity;
 
 public class ContactUsActivity extends AppCompatActivity {
 
+    ImageView back;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.ac_contactus);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        back = (ImageView) toolbar.findViewById(R.id.ivBack);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
